43d8624af05b3960754cd10c8ab185d60a3ddc04
fars-co <89486225+fars-co@users.noreply.github.com>
2021-08-30 19:39:07 +0430

    uploaded new

---
+++ js/mainscript.js
@@ -0,0 +1,24 @@
+
+function load_home() {
+    document.getElementById("leftDiv").innerHTML = '<object type="text/html" data="index.html" ></object>';
+}
+
+function create_document() {
+    document.getElementById("leftDiv").innerHTML = '<object type="text/html" data="register-document.html" ></object>';
+}
+
+function load_items_panel() {
+    document.getElementById("rightDiv").innerHTML = '<object type="text/html" data="main-items-panel.html" ></object>';
+}
+function exit_app() {
+    // var txt;
+    var r = confirm("آیا مطمئن به خروج هستید؟")
+    if (r == true) {
+        var targetURL = "../login2/login.html";
+        window.location = targetURL;
+    }
+    else {
+        return;
+    }
+
+}
